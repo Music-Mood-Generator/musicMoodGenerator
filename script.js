f76ad3940a27874551bcd70187502045964fc44f
Misaki <tshimizumisaki@gmail.com>
2021-12-19 11:26:24 -0500

    added JavaScript file :sparkles:

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create an app object. (moodApp)
+ const moodApp = {};
+// Create variables to hold apiKey, apiURL, userQuery 
+
+// Create separate arrays for each mood holding backgrounds and potential artists/genre
+
+// A landing page greeting users with app title, and welcome message. 
+
+// Form that includes user interaction. User will select from a drop down list of moods. *Use select element* (happy, sad, chill, energetic, romantic) 
+
+// At the bottom of the form, user will click a submit button to see results. 
+
+// Store user selections as a parameter. 
+
+// When user submits data, make AJAX request to retrieve data based on the user mood selection to show recommended artist/genre. 
+
+// If successful, a new page will appear with the users mood playlist/artist, rendering information about the song and music video to the HTML.
+// If unsuccessful, display an error message
+
+// Create an init method to initialize the app
